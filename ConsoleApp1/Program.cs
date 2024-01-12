22f675d09133db6998e5b8510d0583f837ce9b39
Maisie Crane-Gray <17CranM1@sixthform.allhallows.net>
2024-01-12 12:46:02 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Square
+    {
+        //attributes
+        public string Name { get; set; }
+        public List Items { get; set; }
+        public string Description { get; set; }
+        public bool Enemy { get; set; }
+        
+        //constructor
+        public Square(string Name, string Description, bool Enemy, List Items)
+        {
+            string name = Name;
+            string description = Description;
+            bool enemy = Enemy;
+            List items = Items;
+        }
+        //methods
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
